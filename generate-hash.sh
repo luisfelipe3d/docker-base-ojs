d7e08b366ba576597fc90e32ede4fab6977ae09b
luisfelipe3d <31942862+luisfelipe3d@users.noreply.github.com>
2021-11-07 16:22:00 -0300

    Update generate-hash.sh

--- generate-hash.sh
+++ generate-hash.sh
@@ -1,9 +1,39 @@
-#!/bin/bash
+!/usr/bin/env bash
+
+# ===============================================================================
+#          FILE:  generate-hash.sh
+#
+#         USAGE:  gerente-hash
+#
+#   DESCRIPTION:  A script to download all versions of OJS and take checksum.
+#
+#    PARAMETERS:
+#
+#  REQUIREMENTS:  
+#     TODO/BUGS:  ---
+#         NOTES:  ---
+#       AUTHORS:  Luis Leal.
+#  ORGANIZATION:  ---
+#       LICENSE:  GPL 3
+#       CREATED:  11/07/2021 16:11:45 CEST
+#       UPDATED:  11/07/2021 16:11:45 CEST
+#      REVISION:  0.1
+#===============================================================================
+
+fileVersions='ojs-versions.txt'
+if [ -f "$fileVersions" ]; then
+    echo "Downloading all versions of ojs from https://pkp.sfu.ca/ojs/ojs_download/"
+    while read p
+    do
+        wget https://pkp.sfu.ca/ojs/download/ojs-$p.tar.gz -P versions/
+    done < "$fileVersions"
+
+fi
 
 # Generate SHA-512 hashes of all versions (or any other extension) files from https://pkp.sfu.ca/ojs/ojs_download/
-for f in *.tar.gz
+for f in ojs-*.tar.gz
 do
     hash=`sha512sum $f`
-    echo "$f  :  $hash"
-    sha512sum $f >> "ojs-hashes.txt"
+    echo "$hash"
+    sha512sum $f >> "CHECKSUMSHA512.txt"
 done
